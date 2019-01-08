153cc649ab2e90d9747dcf732ad94a7aba07e90b
Chris Magnuson <hourglasssand@gmail.com>
2019-01-08 11:45:11 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Paylocity WebClock Kiosk",
+   "version": "0.3",
+   "homepage": "https://webtime2.paylocity.com/WebTime/Login/WebClock",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "rotation": "0",
+   "kioskEnabled": true
+};
